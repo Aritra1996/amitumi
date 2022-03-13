b076f8b048f3279557c3c075c2312941d985ac55
Aritra1996 <arakshit.engineer@gmail.com>
2022-03-13 07:54:53 +0530

    Added images

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ function App() {
   return (
     <div className="App">
       <div className="gallery">
-      <a target="_blank" href="./images/image_1.jpeg">
-        <img src="./images/image_1.jpeg" alt="Cinque Terre" width="600" height="400"/>
-      </a>
+        <a target="_blank" href="./images/image_1.jpeg">
+          <img src="./images/image_1.jpeg" alt="Cinque Terre" width="600" height="400"/>
+        </a>
       
-    </div>
+      </div>
 
-    <div className="gallery">
-      <a target="_blank" href="./images/image_2.jpeg">
-        <img src="./images/image_2.jpeg" alt="Forest" width="600" height="400"/>
-      </a>
-      
-    </div>
+      <div className="gallery">
+        <a target="_blank" href="./images/image_2.jpeg">
+          <img src="./images/image_2.jpeg" alt="Forest" width="600" height="400"/>
+        </a>
+        
+      </div>
 
-    <div className="gallery">
-      <a target="_blank" href="./images/image_3.jpeg">
-        <img src="./images/image_3.jpeg" alt="Northern Lights" width="600" height="400"/>
-      </a>
-      
-    </div>
+      <div className="gallery">
+        <a target="_blank" href="./images/image_3.jpeg">
+          <img src="./images/image_3.jpeg" alt="Northern Lights" width="600" height="400"/>
+        </a>
+        
+      </div>
 
-    <div className="gallery">
-      <a target="_blank" href="./images/image_4.jpeg">
-        <img src="./images/image_4.jpeg" alt="Mountains" width="600" height="400"/>
-      </a>
-      
-    </div>
+      <div className="gallery">
+        <a target="_blank" href="./images/image_4.jpeg">
+          <img src="./images/image_4.jpeg" alt="Mountains" width="600" height="400"/>
+        </a>
+        
+      </div>
     </div>
   );
 }
